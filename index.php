ce26be4caf821edda641f1ea0a25489844177d61
Thomas <thomas.dewit@outlook.fr>
2019-02-25 10:59:04 +0100

    début html

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <html lang="fr">
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Projet Back-End</title>
+    </head>
+
+    <body>
+    
+    </body>
+</html>
